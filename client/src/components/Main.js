05d652b169169fc5b2a2d417077d3da3abecdba3
sandip-karma <karmasandeep999@gmail.com>
2023-05-05 20:09:54 +0530

    Update Quizzy

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,12 @@
 import React, { useRef } from "react";
-
+import { Link } from "react-router-dom";
+import "../styles/Main.css";
 export default function Main() {
 
     const inputRef = useRef(null)
   return (
     <div className="container">
-      <h1 className="title text-light">Quizzy</h1>
-      <h4>
+      <div className="title text-light">Quizzy - A Quiz Application</div>
         <ol>
           <li>You will be asked some questions one after another.</li>
           <li>1 point is awarded for the correct answer.</li>
@@ -16,10 +16,13 @@ export default function Main() {
           <li>You can review and change answer before the quiz finish.</li>
           <li>The result will be declared at the end of the Quiz.</li>
         </ol>
-      </h4>
       <form id="form">
-        <input ref={inputRef} type="text" placeholder="Username"/>
+        <input ref={inputRef} className="userid" type="text" placeholder="Username"/>
       </form>
+
+      <div className="start">
+      <Link className='btn' to={'quiz'}>Start Quiz</Link>
+      </div>
     </div>
   );
 }
--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Quiz() {
   return (
-    <div>Quiz</div>
+    <div className="container">
+    <h1 className="title text-light">Quizzy - A Quiz Application</h1>
+
+    {/* Display Questions */}
+
+    <div className='grid'>
+      <button>Prev</button>
+      <button>Next</button>
+    </div>
+
+    </div> 
   )
 }
